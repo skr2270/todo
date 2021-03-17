9c49229b75f67afa9d5fff662f99b0bebb8363c2
Sai Kumar Rayavarapu <sk.r.2270@gmail.com>
2021-03-17 15:57:44 +0530

    Added feature to generate todo dynamically

---
+++ js/app.js
@@ -0,0 +1,33 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+//Functions
+function addTodo(event) {
+    console.log("hello");
+    event.preventDefault(); // To prevent default behaviour //Prevent form from submitting
+    //Todo Div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //Create li
+    const todoLi = document.createElement('li');
+    todoLi.innerText = 'hey';
+    todoLi.classList.add('todo-item');
+    todoDiv.appendChild(todoLi);
+    //Check Mark Button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //Check Trash Button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //Append to UL list
+    todoList.appendChild(todoDiv);
+}
